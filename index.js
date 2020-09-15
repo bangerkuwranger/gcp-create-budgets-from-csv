f06ee7acd40e88683661606c0129488bca07aea1
Chad A. Carino <artist@chadacarino.com>
2020-09-15 09:09:46 -0600

    add exports to index, and more defaults

--- index.js
+++ index.js
@@ -77,3 +77,42 @@ const getDefaultThresholds = function() {
 	];
 };
 
+// Parser settings
+const getDefaultParserSettings = function() {
+	return {
+		delimiter: "",	// auto-detect
+		newline: "",	// auto-detect
+		quoteChar: '"',
+		escapeChar: '"',
+		header: true,
+		transformHeader: undefined,
+		dynamicTyping: false,
+		preview: 0,
+		encoding: "",
+		worker: false,
+		comments: false,
+		step: undefined,
+		complete: undefined,
+		error: undefined,
+		download: false,
+		downloadRequestHeaders: undefined,
+		downloadRequestBody: undefined,
+		skipEmptyLines: false,
+		chunk: undefined,
+		chunkSize: undefined,
+		fastMode: undefined,
+		beforeFirstChunk: undefined,
+		withCredentials: undefined,
+		transform: undefined,
+		delimitersToGuess: [',', '\t', '|', ';', Papa.RECORD_SEP, Papa.UNIT_SEP]
+	};
+};
+
+function parseCSVTest(csvStr) {
+	return Papa.parse(csvStr, getDefaultParserSettings());
+}
+
+
+module.exports = {
+	parseCSVTest
+}
